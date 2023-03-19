25a3ec4c7b5bf59c502baf4d12f599cb974a4691
youssef <outikni.youssef97@gmail.com>
2023-03-18 17:48:38 -0700

    to 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,11 @@
 int main(void)
 {
 	char letter = 'a';
-	
 	while (letter <= 'z')
-    {
-	    putchar(letter);
-	    letter++;
-    }
+	{
+		putchar(letter);
+		letter++;
+	}
 
 	putchar('\n');
 
