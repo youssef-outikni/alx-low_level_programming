899ddd8a1524247a78a346a64e504e9515a73f8b
youssef <outikni.youssef97@gmail.com>
2023-03-20 16:18:39 -0700

    to 7

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	char LETTER = 'A';
+	
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	while (LETTER <= 'Z')
+	{
+		putchar(LETTER);
+		LETTER++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
