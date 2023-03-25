3b610415cb8aa9071500cd0dda086d5f53726a67
youssef <outikni.youssef97@gmail.com>
2023-03-24 18:11:39 -0700

    4rd task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,31 @@
 
 #include <stdio.h>
 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 
 int _isupper(int c);
 int _isdigit(int c);
