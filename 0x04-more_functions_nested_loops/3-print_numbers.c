3da08b3fd23a7d09222bd6ab4f326f6c37da7650
youssef <outikni.youssef97@gmail.com>
2023-03-24 18:09:55 -0700

    4rd task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers from 0 to 9.
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
