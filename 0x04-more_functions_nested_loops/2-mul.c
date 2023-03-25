3de16a06a59013fbe51b5b28b5325c7f9ef3a328
youssef <outikni.youssef97@gmail.com>
2023-03-24 17:32:33 -0700

    3rd task 0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - function that multiplies two integers.
+ * @a and b : integers to multiply 
+ *
+ * Return: always a*b
+ */
+
+int mul(int a, int b)
+{
+		return (a*b);
+}
