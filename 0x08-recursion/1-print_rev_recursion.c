994f829ed17199408611c9a633c149660050d2f5
youssef <outikni.youssef97@gmail.com>
2023-04-05 19:12:30 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,8 +10,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);	
+		_print_rev_recursion(s + 1);
 	}
 	s--;
-	_putchar(*s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+	}
 }
