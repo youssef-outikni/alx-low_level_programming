f9dd27ca01ea9531dd7791a69bef7c746e7b9cfe
youssef <outikni.youssef97@gmail.com>
2023-04-08 19:07:42 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: input value.
+ * @argv: input value.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s", *argv++);
+		printf("\n");
+	}
+	return (0);
+}
