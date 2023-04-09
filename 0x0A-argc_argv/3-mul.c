f87359dfae8704c7e7788ed009686b97b9c7e9cd
youssef <outikni.youssef97@gmail.com>
2023-04-08 19:13:29 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: input value.
+ * @argv: input value.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error");
+		return (0);
+	}
+	printf("%d\n", *argv[0] * *argv[1]);
+	return (0);
+}
