85d9094571d097fd1161e3dea1d51f4e62f70586
youssef <outikni.youssef97@gmail.com>
2023-04-08 17:54:08 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+       	__putchar(argv[0]);
+	return (0);
+}
