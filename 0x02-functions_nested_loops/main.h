448d40f465ce0d524c1eb0b157eb158d250dfcf8
youssef <outikni.youssef97@gmail.com>
2023-04-10 11:25:06 -0700

    task 1 in 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,26 +1,6 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+int _putchar(char c);
 
-
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-
+#endif
