8fa66974380331dc11c9a1f4e5e210eb71afd617
youssef <outikni.youssef97@gmail.com>
2023-04-10 03:16:43 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,25 +19,26 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	cents = atoi(argv[1]);
+	printf("%d\n",cents);
 
 	while (cents)
 	{
-		if (cents > 25)
+		if (cents >= 25)
 		{
 			coins = coins +  cents / 25;
 			cents = cents % 25;
 		}
-		else if (cents > 10)
+		else if (cents >= 10)
 		{
 			coins = coins + cents / 10;
 			cents = cents % 10;
 		}
-		else if (cents > 5)
+		else if (cents >= 5)
 		{
 			coins = coins + cents / 5;
 			cents = cents % 5;
 		}
-		else if ( cents > 2)
+		else if ( cents >= 2)
 		{
 			coins = coins + cents / 2;
 			cents = cents % 2;
