e95bf324a5d2d252a659deca7ba0dd016081384f
youssef <outikni.youssef97@gmail.com>
2023-04-11 16:01:37 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *text = (char *) malloc(size);
-
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	if (text != NULL)
 	{
 		for (i = 0; i < size; i++)
