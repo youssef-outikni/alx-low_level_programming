21cc7489d8cdab2e29a5eb91dbade7383c42b090
youssef <outikni.youssef97@gmail.com>
2023-04-13 06:25:45 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <strdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: input value
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *output = malloc(b)
+	return (b);
+}
+
