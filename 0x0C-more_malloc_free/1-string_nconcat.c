d0177f5b7a243538cd9e14f5e431a57cb6dbdcb5
youssef <outikni.youssef97@gmail.com>
2023-04-14 16:08:18 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,11 +44,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = '';
+		size1 = 0;
 	}
 	if (s2 == NULL)
 	{
-		s2 = '';
+		n = 0;
 	}
 	size1 = sizer(s1);
 	if ((unsigned int) sizer(s2) < n)
