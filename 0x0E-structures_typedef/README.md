5050a9f75a22d9256216ec0e82bf249ccebd1a57
youssef <outikni.youssef97@gmail.com>
2023-04-16 20:48:50 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+this is readme file for 0x0E-structures_typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - define a new structure.
+ * @name: name of dog
+ * @age: age of dog 
+ * @owner: owner of dog
+ *
+ * Description: nothing
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
