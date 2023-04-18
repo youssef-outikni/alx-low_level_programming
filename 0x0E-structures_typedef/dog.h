60a35842c0f5cbfe34800188335b14d87a2deabf
youssef <outikni.youssef97@gmail.com>
2023-04-18 16:23:11 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,13 @@
  * Description: nothing
  */
 
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
