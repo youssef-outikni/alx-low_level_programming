c92b6119f82d5183e4db33f97b8682666e6ea157
youssef <outikni.youssef97@gmail.com>
2023-04-20 20:18:16 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+/**
+ * array_iterator - prints array
+ * @array: inputs array
+ * @size: size of array
+ * @action: action to perform for each array
+ *
+ * Return: nothing
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
