ddd741274ac1b0075965ed588e4cab93d7437fa5
youssef <outikni.youssef97@gmail.com>
2023-05-01 15:41:51 -0700

    task 10 correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tp = tp->next;
 		i++;
 	}
-	item = tp-next;
+	item = tp->next;
 	tp->next = item->next;
 	free(item);
 	return (1);
