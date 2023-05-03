0646ece0f81272fded13f3b25b54604bc6220bdd
youssef <outikni.youssef97@gmail.com>
2023-05-03 14:31:53 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - returns the length of a string
+ * @str:input value
+ *
+ * Return: length of the string
+ */
+
+int _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
