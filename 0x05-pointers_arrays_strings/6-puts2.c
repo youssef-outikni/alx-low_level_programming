c49569d649a34cdc2ffdebeb50a1f5e15b4fffff
youssef <outikni.youssef97@gmail.com>
2023-05-03 16:22:46 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - returns the length of a string
+ * @str:input value
+ *
+ * Return: length of the string
+ */
+
+void puts2(char *str)
+{
+	int stg = 0;
+	int i = 0;
+	int a;
+	char *s = str;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	stg = i-1;
+	for (a = 0; a < stg; a++)
+	{
+		if (a%2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
