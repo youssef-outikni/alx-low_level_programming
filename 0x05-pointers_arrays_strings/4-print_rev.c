9c6d39f7e2de5bda3144a344ac18d7ed5fd5c1ed
youssef <outikni.youssef97@gmail.com>
2023-05-03 16:04:55 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - returns the length of a string
+ * @s:input value
+ *
+ * Return: length of the string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (a = i; a > 0; a++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
